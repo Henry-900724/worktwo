234ca8f11b3b0d345459417ed5390f6f1bada13f
莊育霖 <q0393962@gmail.com>
2025-11-18 19:05:29 +0800

    Initial Commit

---
+++ worktwo/worktwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  worktwoApp.swift
+//  worktwo
+//
+//  Created by 莊育霖 on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct worktwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
